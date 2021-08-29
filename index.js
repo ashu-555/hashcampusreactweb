0626d9f577d671f26f5b8b78bfae69c724411878
Ashutosh singh <88020230+Ashutoshsingh555@users.noreply.github.com>
2021-08-29 22:30:57 +0530

    first commit

--- index.js
+++
@@ -1,6 +0,0 @@
-// Making Build Folder as Public 
-app.use(express.static(path.join(__dirname, 'build')));
-
-app.get('/', function(req, res) {
-  res.sendFile(path.join(__dirname, 'build', 'index.html'));
-});
